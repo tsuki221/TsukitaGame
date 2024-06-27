a20a90eaffaf6dddc879aefbe997a39baabc55a9
tsukita <s19116230@stu.edu.tw>
2024-06-27 12:31:25 -0400

    Input v1

--- main.js
+++ main.js
@@ -12,44 +12,74 @@ const { Application, Assets, Sprite } = PIXI;
   document.querySelector("#game-container").appendChild(app.canvas);
 
   const texture = await Assets.load("https://pixijs.com/assets/bunny.png");
-  
 
-  const numberOfBunnies = 1
-  new Array(numberOfBunnies).fill(null).forEach(element => {
-    element = new MiBunny(texture, app)
+  const numberOfBunnies = 1;
+  // Hacer muchos conejos
+  new Array(numberOfBunnies).fill(null).forEach((element) => {
+    element = new MiBunny(texture, app);
   });
-
 })();
 
 class MiBunny {
-  texture
-  sprite
-  constructor(texture, app){
-    this.texture = texture
-    this.sprite = new Sprite(texture)
+  // Variables
+  app;
+  texture;
+  sprite;
+  groundPosition = 1000
+  isInGround = false;
+
+  constructor(texture, app) {
+    this.app = app;
+    this.texture = texture;
+    this.sprite = new Sprite(texture);
     app.stage.addChild(this.sprite);
-  
-  
+
+    // Initial position
     this.sprite.anchor.set(0.5);
-
     this.sprite.x = random(
-      this.sprite.width, 
-      app.screen.width - this.sprite.width);
-
+      this.sprite.width,
+      app.screen.width - this.sprite.width
+    );
     this.sprite.y = 0;
 
     const onUpdate = (time) => {
-      if (this.sprite.y >= app.screen.height - (this.sprite.height / 2)) {
-        this.sprite.angle = 180;
+      this.groundPosition = app.screen.height - this.sprite.height / 2;
+      this.isInGround = this.sprite.y >= this.groundPosition;
+
+      if (this.isInGround) {
+        this.sprite.y = this.groundPosition;
+        // this.sprite.angle = 180;
       } else {
-        this.sprite.angle += 6* time.deltaTime;
+        // Fall
+        this.sprite.angle += 6 * time.deltaTime;
         this.sprite.y += 1.5 * time.deltaTime;
       }
-    }
+    };
     app.ticker.add(onUpdate);
+    this.startInput();
   }
-} 
+
+  startInput() {
+    const resetAngle = () =>{
+      if (this.isInGround) {
+        this.sprite.angle = 0;
+      }
+    }
+
+    // Input Event
+    addEventListener("keydown", (event) => {
+      if (event.key === "a") {
+        this.sprite.x -= 10;
+        resetAngle();
+      } // Move left
+      if (event.key === "d") {
+        this.sprite.x += 10;
+        resetAngle();
+      } // Move right
+    });
+  }
+}
 
 const random = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1 ) + min)
-}
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
