d6d99d16307bf67c61b636fa9a7718b03e149e4f
Tsukugi <Tsukugi@proton.me>
2024-06-28 06:50:30 +0200

    Physics v1

--- src/bunny.mjs
+++ src/bunny.mjs
@@ -9,12 +9,16 @@ export class MiBunny {
     sprite;
     groundPosition = 1000
     isInGround = false;
-    inputEvent = null
+    inputAxis = {
+        x: 0,
+        y: 0,
+    }
 
+    YForce = 0;
     gravity = 3.5;
 
     velocity = 10;
-    jumpVelocity = 50;
+    jumpVelocity = 4;
 
     constructor(texture, app) {
         this.app = app;
@@ -38,60 +42,89 @@ export class MiBunny {
         this.groundPosition = this.app.screen.height - this.sprite.height / 2;
         this.isInGround = this.sprite.y >= this.groundPosition;
 
-        if (this.isInGround) {
-            switch (this.inputEvent) {
-                case "a":
-                case "ArrowLeft":    
-                    return this.moveX(-this.velocity, time);
-                case "d":
-                case "ArrowRight": 
-                     return this.moveX(this.velocity, time);
+        if (!this.isInGround) {
+            this.addGravity(time);
+            this.fallAnimation(time);
+        } else {
+            if (this.inputAxis.x != 0) this.resetAngle();
+        }
 
-                case " ":
-                case "ArrowUp":
-                    return this.moveY(-this.jumpVelocity, time);
+        this.addYForce(this.jumpVelocity * this.inputAxis.y, time);
+        this.moveX(this.velocity * this.inputAxis.x, time);
+        this.placeInWindow();
 
-                case "s":
-                case "ArrowDown":
-                    return this.moveY(this.jumpVelocity, time);
-                default:
-                    this.sprite.y = this.groundPosition;
-            }
-            // this.sprite.angle = 180;
-        } else this.fall(time);
+    }
+
+    placeInWindow() {
+        const offset = this.sprite.height / 2;
+        const { height, width } = this.app.screen;
+        if (this.sprite.x > width) this.sprite.x = offset;
+        if (this.sprite.x < 0) this.sprite.x = width - offset;
+        if (this.sprite.y > height) this.sprite.y = height - offset;
+        if (this.sprite.y < 0) this.sprite.y = offset;
+    }
+
+    addYForce(velocity, time) {
+        this.YForce += (velocity - this.gravity) * time.deltaTime;
+        if (this.YForce <= 0) this.YForce = 0;
+
+        this.sprite.y -= this.YForce;
+    }
+
+    addGravity(time) {
+        this.sprite.y += this.gravity * time.deltaTime;
     }
 
     moveX(velocity, time) {
-        console.log(velocity)
         this.sprite.x += velocity * time.deltaTime;
     }
+
     moveY(velocity, time) {
         this.sprite.y += velocity * time.deltaTime;
     }
 
-    fall(time) {
+    fallAnimation(time) {
         this.sprite.angle += 6 * time.deltaTime;
-        this.sprite.y += this.gravity * time.deltaTime;
     }
 
+    resetAngle() {
+        this.sprite.angle = 0;
+    }
 
     startInput() {
-        const resetAngle = () => {
-            if (this.isInGround) {
-                this.sprite.angle = 0;
-            }
-        }
-
         // Input Event
         addEventListener("keydown", (event) => {
-            this.inputEvent = event.key;
-            console.log(this.inputEvent);
-            resetAngle();
+            switch (event.key) {
+                case "a":
+                case "ArrowLeft":
+                    this.inputAxis.x = -1; break;
+                case "d":
+                case "ArrowRight":
+                    this.inputAxis.x = 1; break;
+                case " ":
+                case "ArrowUp":
+                    this.inputAxis.y = 1; break;
+                case "s":
+                case "ArrowDown":
+                    this.inputAxis.y = -1; break;
+            }
         });
 
         addEventListener("keyup", (event) => {
-            if (event.key != this.inputEvent) return;
-            this.inputEvent = null;
+            switch (event.key) {
+                case "a":
+                case "ArrowLeft":
+                    this.inputAxis.x = 0; break;
+                case "d":
+                case "ArrowRight":
+                    this.inputAxis.x = 0; break;
+                case " ":
+                case "ArrowUp":
+                case "s":
+                case "ArrowDown":
+                    this.inputAxis.y = 0; break;
+            }
+            console.log(this.inputAxis)
         });
     }
 }
